5be3ab702c4d81b222a342e5767250ffa770a4ea
alishma <alishma001@gmail.com>
2023-08-03 14:28:30 +0545

    updated

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -3,23 +3,37 @@ import Navbar from '../home/Navbar';
 
 function Contact() {
     return (
-      <>
-        <div className={Styles.header}>
-            <div className={Styles.navigation}>
-                <Navbar />
-                <div className={Styles.hero_section}>
-                    <div className={Styles.title}>discover your perfect website <br /> template</div>
-                    <div className={Styles.wellcome_message}>
-                        Welcome to Darkprog, where creativity meets functionality. We offer a curated <br /> 
-                        collection of exquisite website templates to cater to all your online needs. 
+        <>
+            <div className={Styles.header}>
+                <div className={Styles.navigation}>
+                    <Navbar />
+                    <div className={Styles.hero_section}>
+                        <div className={Styles.title}>Contact Us </div>
                     </div>
-                    <button className={Styles.button}>Browse Template</button>
+                </div>
+
+                <div className={Styles.Section}>
+                    <div className={Styles.heading}>
+                        Get In Touch With Us.
+                    </div>
+
+                    <div className={Styles.content}>
+                        <div className='info'>
+                            <p>Feel free to get in touch with us for any question, feedback, or support. We're here to assits you!</p>
+                            <h5>Phone Number: <span>+977 9832487278</span></h5>
+                            <h5>Address: <span>Pokhara-26</span></h5>
+                            <h5>Phone Number: <span>+977 9832487278</span></h5>
+                            <div>
+                                <i class="fa-brands fa-square-facebook"></i>
+                            </div>
+                        </div>
+
+                    </div>
+
                 </div>
             </div>
-        </div>
-      </>
+        </>
     );
-  }
+}
 
 export default Contact;
-  
