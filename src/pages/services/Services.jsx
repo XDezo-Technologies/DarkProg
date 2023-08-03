aa9c22deb15a44ecfc54630052b2ebf6bc02d36e
prazal404prazal404 <prazal05@gmail.com>
2023-08-03 14:25:58 +0545

    falink

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -2,24 +2,51 @@ import Navbar from '../home/Navbar';
 import Styles from './Services.module.css'
 
 function Services() {
-    return (
-      <>
-        <div className={Styles.header}>
-            <div className={Styles.navigation}>
-                <Navbar />
-                <div className={Styles.hero_section}>
-                    <div className={Styles.title}>Our Servises</div>
-                    {/* <div className={Styles.wellcome_message}>
-                        Welcome to Darkprog, where creativity meets functionality. We offer a curated <br /> 
-                        collection of exquisite website templates to cater to all your online needs. 
-                    </div>
-                    <button className={Styles.button}>Browse Template</button> */}
-                </div>
-            </div>
+  return (
+    <>
+      <div className={Styles.header}>
+        <div className={Styles.navigation}>
+          <Navbar />
+          <div className={Styles.hero_section}>
+            <div className={Styles.title}>Our Services</div>
+          </div>
         </div>
-      </>
-    );
-  }
+      </div>
+      <div className={Styles.Content}>
+        <div className={Styles.Contentm}>
+          <div className={Styles.title1}>
+            <h1>Services</h1>
+            <div className={Styles.box}>
+              <div className={Styles.box1}>
+              <i class="fa-solid fa-mobile-screen-button"></i>
+                <h2>Consulation</h2>
+                <p>In publishing and graphic design, Lorem ipsum is a <br />placeholder text commonly used to demonstrate the visual form of a document<br /> or a typeface without relying on meaningful content.</p>
+
+              </div>
+              <div className={Styles.box2}>
+              <i class="fa-solid fa-lightbulb"></i>
+                <h2>Best Ideas</h2>
+                <p>In publishing and graphic design, Lorem ipsum is a <br/>placeholder text commonly used to demonstrate the visual form of a document or<br/> a typeface without relying on meaningful content.</p>
+
+              </div>
+              <div className={Styles.box3}>
+              <i class="fa-solid fa-gear"></i>
+                <h2>Simple Setting</h2>
+                <p>In publishing and graphic design, Lorem ipsum is a <br/>placeholder text commonly used to demonstrate the visual form of a document or<br/> a typeface without relying on meaningful content.</p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='Categories'>
+
+
+      </div>
+
+
+    </>
+  );
+}
 
 export default Services;
-  
