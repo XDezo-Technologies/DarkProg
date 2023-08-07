a860170c8b4c6873955f75bc766f0350a3288b91
prazal404prazal404 <prazal05@gmail.com>
2023-08-07 11:55:46 +0545

    services

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -13,33 +13,51 @@ function Services() {
         </div>
       </div>
       <div className={Styles.Content}>
-        <div className={Styles.Contentm}>
           <div className={Styles.title1}>
             <h1>Services</h1>
             <div className={Styles.box}>
               <div className={Styles.box1}>
-              <i class="fa-solid fa-mobile-screen-button"></i>
+                <i class="fa-solid fa-mobile-screen-button"></i>
                 <h2>Consulation</h2>
                 <p>In publishing and graphic design, Lorem ipsum is a <br />placeholder text commonly used to demonstrate the visual form of a document<br /> or a typeface without relying on meaningful content.</p>
 
               </div>
               <div className={Styles.box2}>
-              <i class="fa-solid fa-lightbulb"></i>
+                <i class="fa-solid fa-lightbulb"></i>
                 <h2>Best Ideas</h2>
-                <p>In publishing and graphic design, Lorem ipsum is a <br/>placeholder text commonly used to demonstrate the visual form of a document or<br/> a typeface without relying on meaningful content.</p>
+                <p>In publishing and graphic design, Lorem ipsum is a <br />placeholder text commonly used to demonstrate the visual form of a document or<br /> a typeface without relying on meaningful content.</p>
 
               </div>
               <div className={Styles.box3}>
-              <i class="fa-solid fa-gear"></i>
+                <i class="fa-solid fa-gear"></i>
                 <h2>Simple Setting</h2>
-                <p>In publishing and graphic design, Lorem ipsum is a <br/>placeholder text commonly used to demonstrate the visual form of a document or<br/> a typeface without relying on meaningful content.</p>
+                <p>In publishing and graphic design, Lorem ipsum is a <br />placeholder text commonly used to demonstrate the visual form of a document or<br /> a typeface without relying on meaningful content.</p>
               </div>
 
             </div>
           </div>
         </div>
-      </div>
-      <div className='Categories'>
+      <div className='Categoriesc'>
+        <div className='Categories'>
+          <h1>Categories</h1>
+        </div>
+        <div className='Catergories1'>
+          <h6>Personal</h6>
+          <h6>Personal</h6>
+          <h6>Personal</h6>
+          <h6>Personal</h6>
+          <h6>Personal</h6>
+
+        </div> 
+        
+
+
+
+
+
+
+
+
 
 
       </div>
