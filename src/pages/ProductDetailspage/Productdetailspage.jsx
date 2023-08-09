5eff11d127ce991be1e5c7914c25316c3c98dea0
prazal404prazal404 <prazal05@gmail.com>
2023-08-09 12:55:41 +0545

    PdP

--- src/pages/ProductDetailspage/Productdetailspage.jsx
+++ src/pages/ProductDetailspage/Productdetailspage.jsx
@@ -8,25 +8,38 @@ function Productdetailspage() {
 
         <div className={Styles.header}>
             <div className={Styles.navigation}>
-                <Navbar backgroundColor="Black"/>
+                <Navbar backgroundColor="Black" />
                 <div className={Styles.productdetails}>
                     <div className={Styles.pdh}>
                         Product / Product Details
                     </div>
                     <div className={Styles.products}>
-                    <div className={Styles.productimg}>
-                        <img src={webproduct} alt="" />
-                    </div>
-                    <div className={Styles.productd}>
-                        <h6>Product Name</h6>
-                        <p>Rs30000</p>
+                        <div className={Styles.productimg}>
+                            <img src={webproduct} alt="" />
+                        </div>
+                        <div className={Styles.productd}>
+                            <h3>Product Name</h3>
+                            <p>Rs30000</p>
 
-                        <button>BuyNow</button> <br/>
-                        
-                        <button>Add to cart</button>
+                            <button>BuyNow</button> <br />
 
-                        <p>Categories: E-commerce</p>
+                            <button>Add to cart</button>
+
+                            <p>Categories: E-commerce</p>
+
+
+                        </div>
                     </div>
+
+                    <div className={Content}>
+                        <div className={content1}>
+                            <h1>Trend Website Design</h1>
+                        </div>
+                        <div className={content2}>
+                            <p>For your business, too, your customers’ wins are your wins. If you’re a small retailer, this is true not just with your in-store customer service, but also your e-commerce website’s overall UX, its check-out flow, your online return options, and your product descriptions. By writing accurate, educational, and engaging product descriptions, a copywriter serves their customers just as much as a sales associate does when someone walks through your shop’s front door. For your business, too, your customers’ wins are your wins. If you’re a small retailer, this is true not just with your in-store customer service, but also your e-commerce website’s overall UX, its check-out flow, your online return options, and your product descriptions. By writing accurate, educational, and engaging product descriptions, a copywriter serves their customers just as much as a sales associate does when someone walks through your shop’s front door.
+                                For your business, too, your customers’ wins are your wins. If you’re a small retailer, this is true not just with your in-store customer service, but also your e-commerce website’s overall UX, its check-out flow, your online return options, and your product descriptions. By writing accurate, educational, and engaging product descriptions, a copywriter serves their customers just as much as a sales associate does when someone walks through your shop’s front door.</p>
+                        </div>
+
                     </div>
 
 
@@ -43,4 +56,5 @@ function Productdetailspage() {
 
     );
 }
+
 export default Productdetailspage;
