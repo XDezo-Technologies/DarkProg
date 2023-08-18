0535536117a78b5666fed15174be2c6bd3bb55b4
prazal404prazal404 <prazal05@gmail.com>
2023-08-18 14:12:00 +0545

    Services

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -44,111 +44,128 @@ function Services() {
           <h1>Categories</h1>
         </div>
         <div className={Styles.Categories1}>
-          <table>
-            <tr>
-              <th>  Personal  <i class="fa-solid fa-caret-down"></i> </th>
-              <th> Entertaiment <i class="fa-solid fa-caret-down"></i></th>
-              <th> Web Development <i class="fa-solid fa-caret-down"></i></th>
-              <th> Portfolio <i class="fa-solid fa-caret-down"></i></th>
-              <th> Technology <i class="fa-solid fa-caret-down"></i> </th>
-            </tr>
-          </table>
+          <div className={Styles.cat1}>
+            <div className={Styles.cat2}>
+              <div className={Styles.name}>Personal</div>  <i class="fa-solid fa-caret-down"></i>
+            </div>
+            <div className={Styles.cat3}>
+            </div>
+
+            <div className={Styles.cat2}>
+              <div className={Styles.name1}>Entertainment</div> <i class="fa-solid fa-caret-down"></i>
+            </div>
+            <div className={Styles.cat5}></div>
+
+            <div className={Styles.cat2}>
+              <div className={Styles.name2}> Web Development</div> <i class="fa-solid fa-caret-down"></i>
+            </div>
+            <div className={Styles.cat7}></div>
+
+            <div className={Styles.cat2}>
+              <div className={Styles.name3}>Portfolio</div> <i class="fa-solid fa-caret-down"></i>
+            </div>
+            <div className={Styles.cat9}>
+            </div>
+
+            <div className={Styles.cat2}>
+              <div className={Styles.name4}>Technology</div>  <i class="fa-solid fa-caret-down"></i>
+            </div>
+            <div className={Styles.cat11}>
+
+            </div>
+
+          </div>
 
         </div>
 
       </div>
 
-        <div className={Styles.Payment}>
-          <div className={Styles.Payment1}>
-            <h1>Payment Plans</h1>
-          </div>
-          <div className={Styles.Mpayment}>
-            <div className={Styles.payment2}>
-              <h3>Free</h3>
-              <p>Rs 0/month</p><br />
-              <p><i class="fa-solid fa-check"></i> Free Membership</p>
-              <p><i class="fa-solid fa-check"></i> 30Gb Storage</p>
-              <p><i class="fa-solid fa-check"></i> 1 Personal Email</p> <br />
-              <button><Link to="/home">Get Started <i class="fa-solid fa-angle-right"></i> </Link></button>
+      <div className={Styles.Payment}>
+        <div className={Styles.Payment1}>
+          <h1>Payment Plans</h1>
+        </div>
+        <div className={Styles.Mpayment}>
+          <div className={Styles.payment2}>
+            <h3>Free</h3>
+            <p>Rs 0/month</p><br />
+            <p><i class="fa-solid fa-check"></i> Free Membership</p>
+            <p><i class="fa-solid fa-check"></i> 30Gb Storage</p>
+            <p><i class="fa-solid fa-check"></i> 1 Personal Email</p> <br />
+            <button><Link to="/home">Get Started <i class="fa-solid fa-angle-right"></i> </Link></button>
 
+          </div>
+
+          <div className={Styles.payment3}>
+            <h3> Premium</h3>
+            <p>Rs 1000/month</p><br />
+            <p><i class="fa-solid fa-check"></i> Free Membership</p>
+            <p><i class="fa-solid fa-check"></i> 1000Gb Storage</p>
+            <p><i class="fa-solid fa-check"></i> 2 Personal Email</p>
+            <p><i class="fa-solid fa-check"></i> 1 FTP Account</p><br />
+
+            <button><Link to="/payment">Get Started <i class="fa-solid fa-angle-right"></i> </Link></button>
+          </div>
+        </div>
+
+
+      </div>
+
+
+      <div className={Styles.Refundpolicy}>
+        <div className={Styles.refund}>
+          <h1>Refund Policy</h1>
+        </div>
+        <div className={Styles.refund1}>
+          <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+
+        </div>
+
+      </div>
+
+      <div className={Styles.qA}>
+        <div className={Styles.qA1}>
+          <h1>Q&A</h1>
+        </div>
+        <div className={Styles.qA2}>
+          <div className={Styles.qa3}>
+
+          <div className={Styles.cat2}>
+              <div className={Styles.name}>What do you want?</div>  <i class="fa-solid fa-caret-down"></i>
+            </div>
+            <div className={Styles.cat3}>
             </div>
 
-            <div className={Styles.payment3}>
-              <h3> Premium</h3>
-              <p>Rs 1000/month</p><br />
-              <p><i class="fa-solid fa-check"></i> Free Membership</p>
-              <p><i class="fa-solid fa-check"></i> 1000Gb Storage</p>
-              <p><i class="fa-solid fa-check"></i> 2 Personal Email</p>
-              <p><i class="fa-solid fa-check"></i> 1 FTP Account</p><br />
+            <div className={Styles.cat2}>
+              <div className={Styles.name1}>What do you want?</div> <i class="fa-solid fa-caret-down"></i>
+            </div>
+            <div className={Styles.cat5}></div>
 
-              <button><Link to="/payment">Get Started <i class="fa-solid fa-angle-right"></i> </Link></button>
+            <div className={Styles.cat2}>
+              <div className={Styles.name2}>What do you want?</div> <i class="fa-solid fa-caret-down"></i>
+            </div>
+            <div className={Styles.cat7}></div>
+
+            <div className={Styles.cat2}>
+              <div className={Styles.name3}>What do you want?</div> <i class="fa-solid fa-caret-down"></i>
+            </div>
+            <div className={Styles.cat9}>
+            </div>
+
+            <div className={Styles.cat2}>
+              <div className={Styles.name4}>What do you want?</div>  <i class="fa-solid fa-caret-down"></i>
+            </div>
+            <div className={Styles.cat11}>
+
             </div>
           </div>
 
 
-        </div>
 
-
-        <div className={Styles.Refundpolicy}>
-          <div className={Styles.refund}>
-            <h1>Refund Policy</h1>
-          </div>
-          <div className={Styles.refund1}>
-            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-
-          </div>
 
         </div>
+      </div>
 
-        <div className={Styles.qA}>
-          <div className={Styles.qA1}>
-            <h1>Q&A</h1>
-          </div>
-          <div className={Styles.qA2}>
-          <table>
-            <tr>
-              <th> <div className="question">What do you want?</div> <i class="fa-solid fa-caret-down"></i> </th>
-              <th> What do you want? <i class="fa-solid fa-caret-down"></i></th>
-              <th> What do you want? <i class="fa-solid fa-caret-down"></i></th>
-              <th> What do you want? <i class="fa-solid fa-caret-down"></i></th>
-              <th> What do you want? <i class="fa-solid fa-caret-down"></i> </th>
-            </tr>
-          </table>
-
-          </div>
-
-          {/* <div className={Styles.Categories1}>
-          <table>
-            <tr>
-              <th>  <div className={Styles.cat}>Personal</div>  <i class="fa-solid fa-caret-down"></i> </th>
-              <th> <div className={Styles.cat}>Entertaiment</div> <i class="fa-solid fa-caret-down"></i></th>
-              <th> <div className={Styles.cat}>Web Development</div> <i class="fa-solid fa-caret-down"></i></th>
-              <th> <div className={Styles.cat}>Portfolio</div> <i class="fa-solid fa-caret-down"></i></th>
-              <th> <div className={Styles.cat}>Technology</div> <i class="fa-solid fa-caret-down"></i> </th>
-            </tr>
-          </table> */}
-
-        {/* </div> */}
-
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-
-
-        <Footer/>
+      <Footer />
 
     </>
   );
