f1e9b14ea70484f78154259515ad84efa436573b
buddhi111 <rajbuddhi496@gmail.com>
2023-08-29 13:32:03 +0545

    mission and vision updated

--- src/pages/Mission/Mission.jsx
+++ src/pages/Mission/Mission.jsx
@@ -10,13 +10,33 @@ function Mission() {
          <div>
             <Navbar backgroundColor="black" />
 
+            <div className={Styles.top}>
+                <h1>Our Mission</h1>
+                <p> At DarkProg, our mission is to empower individuals and businesses with the tools they need to establish a compelling online presence. We understand that in today's digital age, a website is more than just a virtual storefront—it's a gateway to opportunities, connections, and growth. At WebCrafters, we believe in the power of partnership. Our mission is to guide our clients throughout their digital voyage, offering expert assistance, customizable solutions, and a seamless journey from website selection to launch.</p>
+            </div>
+
             <div className={Styles.image}>
                 <img src={mission} alt="" />
             </div>
 
             <div className={Styles.heading}>
-                <h1>Our Mission</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid fugiat laborum, earum ab repudiandae inventore vel impedit minus dolorem fugit, soluta quas quisquam, itaque ut ea dicta eaque! Nesciunt qui recusandae nihil excepturi veniam consequuntur aliquid nisi inventore iste, obcaecati officiis est perferendis aliquam numquam quas! Quo ullam expedita, nesciunt cum inventore commodi natus quidem eligendi molestias unde quis iusto quam, minima ab officia quibusdam quia ut dolorum corrupti pariatur!</p>
+                <h1>Crafting Digital Identities:</h1>
+                <p>We are dedicated to crafting digital identities that resonate with our clients' visions. Our mission is to provide meticulously designed and user-friendly websites that not only reflect the essence of each business but also engage and captivate visitors.</p>
+            </div>
+
+            <div className={Styles.heading}>
+                <h1>Elevating Online Experiences:</h1>
+                <p>Our commitment goes beyond aesthetics. We strive to elevate online experiences by offering websites that are not only visually appealing but also functionally robust. Our mission is to empower our clients with websites that load seamlessly, adapt to different devices, and provide an exceptional user journey.</p>
+            </div>
+
+            <div className={Styles.heading}>
+                <h1>Guiding Your Digital Voyage:</h1>
+                <p>At DarkProg, we believe in the power of partnership. Our mission is to guide our clients throughout their digital voyage, offering expert assistance, customizable solutions, and a seamless journey from website selection to launch.</p>
+            </div>
+
+            <div className={Styles.heading}>
+                <h1>Join Us in Redefining the Web:</h1>
+                <p>We invite you to be part of our mission in redefining the web. Whether you're an entrepreneur with a vision, a creative professional seeking exposure, or an organization aiming to expand your digital footprint, WebCrafters is here to be your companion in the digital realm. <br /><div className={Styles.itallen}>Empowerment, Innovation, Excellence—Welcome to the WebCrafters Mission.</div></p>
             </div>
         </div>
         <Footer/>
--- src/pages/Vission/Vision.jsx
+++ src/pages/Vission/Vision.jsx
@@ -1,25 +1,53 @@
 import React from 'react'
 import Navbar from '../home/Navbar'
-import Styles from './Vision.module.css';
+import Styles from '../Mission/Mission.module.css';
 import vision from '../../images/vision.png';
 import Footer from '../home/Footer';
 
 export default function Vision() {
     return (
         <>
+<>
          <div>
             <Navbar backgroundColor="black" />
+
+            <div className={Styles.top}>
+                <h1>Our Vision</h1>
+                <p> At DarkProg, we envision a digital landscape where the process of owning a website is a delightful and accessible experience for individuals and businesses alike. Our vision is to provide a platform that simplifies the complexities of website creation. We see a world where individuals, regardless of their technical background, can effortlessly choose, customize, and launch their websites with confidence.</p>
+            </div>
 
             <div className={Styles.image}>
                 <img src={vision} alt="" />
             </div>
 
             <div className={Styles.heading}>
-                <h1>Our Vision</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid fugiat laborum, earum ab repudiandae inventore vel impedit minus dolorem fugit, soluta quas quisquam, itaque ut ea dicta eaque! Nesciunt qui recusandae nihil excepturi veniam consequuntur aliquid nisi inventore iste, obcaecati officiis est perferendis aliquam numquam quas! Quo ullam expedita, nesciunt cum inventore commodi natus quidem eligendi molestias unde quis iusto quam, minima ab officia quibusdam quia ut dolorum corrupti pariatur!</p>
+                <h1>Innovation Driving Accessibility:</h1>
+                <p>We are dedicated to crafting digital identities that resonate with our clients' visions. Our mission is to provide meticulously designed and user-friendly websites that not only reflect the essence of each business but also engage and captivate visitors.</p>
+            </div>
+
+            <div className={Styles.heading}>
+                <h1>Unleashing Creative Potential:</h1>
+                <p>Our commitment goes beyond aesthetics. We strive to elevate online experiences by offering websites that are not only visually appealing but also functionally robust. Our mission is to empower our clients with websites that load seamlessly, adapt to different devices, and provide an exceptional user journey.</p>
+            </div>
+
+            <div className={Styles.heading}>
+                <h1>Community and Collaboration:</h1>
+                <p>At DarkProg, we believe in the power of partnership. Our mission is to guide our clients throughout their digital voyage, offering expert assistance, customizable solutions, and a seamless journey from website selection to launch.</p>
+            </div>
+
+            <div className={Styles.heading}>
+                <h1>Empowering Growth:</h1>
+                <p>At DarkProg, we believe in the power of partnership. Our mission is to guide our clients throughout their digital voyage, offering expert assistance, customizable solutions, and a seamless journey from website selection to launch.</p>
+            </div>
+
+            <div className={Styles.heading}>
+                <h1>Join Us in Redefining the Web:</h1>
+                <p>We invite you to be part of our mission in redefining the web. Whether you're an entrepreneur with a vision, a creative professional seeking exposure, or an organization aiming to expand your digital footprint, WebCrafters is here to be your companion in the digital realm. <br /><div className={Styles.itallen}>Empowerment, Innovation, Excellence—Welcome to the WebCrafters Mission.</div></p>
             </div>
         </div>
         <Footer/>
+        
+        </>
         
         </>
        
