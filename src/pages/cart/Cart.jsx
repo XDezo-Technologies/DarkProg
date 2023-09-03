b642d48eb958b9eea7c81e101155562b608d74cd
buddhi111 <rajbuddhi496@gmail.com>
2023-09-03 18:23:07 +0545

    cart ui changed

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -5,31 +5,33 @@ import { useNavigate } from 'react-router-dom';
 
 function Cart({ cart }) {
   const navigate = useNavigate();
+
     return (
       <>
       <Navbar backgroundColor="Black" />
       <div className='cart'>
-      <div className='cart_back'><div onClick={() => navigate(-1)}>Product details</div><i class="fa-solid fa-angle-right"></i><span> Payment </span> </div>
-        <div>
-          <h1>Your Cart</h1>
-          <ul>
-            {cart.map((product, index) => (
-              <li key={index}>
-                <div className="cart_products">
-                  <img className="cart_image" src={product.image} alt="" />
-                  <div className="cart_product_detail">
-                    <div className="cart_product_name">{product.title}</div>
-                    <div className="cart_product_cost">{product.cost}</div>
+        <div className='cart_back'><div onClick={() => navigate(-1)}>Product details</div><i class="fa-solid fa-angle-right"></i><span> Payment </span> </div>
+          <div>
+            <h1>Your Cart</h1>
+            <ul>
+              {cart.map((product, index) => (
+                <li key={index}>
+                  <div className="cart_products">
+                    <img className="cart_image" src={product.image} alt="" />
+                    <div className="cart_product_detail">
+                      <div className="cart_product_name">{product.title}</div>
+                      <div className="cart_product_cost"><span>{product.cost}</span></div>
+                    </div>
+                    <div className="cart_action">
+                    <button>Remove</button>
                   </div>
-                  <div className="cart_action">
-                  <button>Remove</button>
-                </div>
-                </div>   
-              </li>
-            ))}
-          </ul>
+                  </div>   
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div></>
+      </>
     );
   }
   
